8761c397d69295a84082568419bc63e0193e5bee
Ram Nellutla <ram.nellutla1@gmail.com>
2020-11-16 22:11:47 -0500

    added serverless support: listing and gloabls mic ser

--- flaskr/listings/listings.py
+++ flaskr/listings/listings.py
@@ -3,26 +3,16 @@ from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import requests
 import json
 
-listings = Flask(__name__)
 
-
-@listings.route('/api/getListings')
-def getListings():
+def getListings(parameters):
     url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+    session = requests.session()
     headers = {
         'Accepts': 'application/json',
         'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
     }
-    session = requests.session()
     session.headers.update(headers)
     try:
-        parameters = {
-            'start': request.args.get('start'),
-            'limit': request.args.get('limit'),
-            'convert': request.args.get('convert'),
-            'sort': request.args.get('sort'),
-            'sort_dir': request.args.get('sort_dir')
-        }
         response = session.get(url, params=parameters)
         data = json.loads(response.text)
         return data
---
+++ flaskr/listings/run.bat
@@ -0,0 +1,4 @@
+cd /D "%~dp0"
+set FLASK_APP=listings.py
+set FLASK_ENV=development
+flask run
